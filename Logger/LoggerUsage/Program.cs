806d80f6b1ead91b36891c15c042a36964991444
Richard Raley <RRaley@inboundtechnologies.com>
2020-09-10 09:02:31 -0500

    Console application to show logging examples

---
+++ Logger/LoggerUsage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RLog;
+namespace LoggerUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            //Creates a Debug Message
+            logger.LogMessage(Logger.LogLevel.DEBUG, "This is a debug message");
+
+
+            // Creates a Info Messsage
+            logger.LogMessage(Logger.LogLevel.INFO, "This is an info message");
+        }
+    }
+}
