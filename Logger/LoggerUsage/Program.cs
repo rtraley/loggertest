f79b2df73cfd55e2cc47d4c4898b44cd49675020
Richard Raley <RRaley@inboundtechnologies.com>
2020-09-10 09:18:37 -0500

    Added fatal error message

--- Logger/LoggerUsage/Program.cs
+++ Logger/LoggerUsage/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using RLog;
+using static RLog.Logger;
+
 namespace LoggerUsage
 {
     class Program
@@ -11,9 +13,14 @@ namespace LoggerUsage
             //Creates a Debug Message
             logger.LogMessage(Logger.LogLevel.DEBUG, "This is a debug message");
 
+            // Creates an Info Messsage
+            logger.LogMessage(LogLevel.INFO, "This is an info message");
 
-            // Creates a Info Messsage
-            logger.LogMessage(Logger.LogLevel.INFO, "This is an info message");
+            // Creates an Error Messsage
+            logger.LogMessage(LogLevel.ERROR, "This is an error message");
+
+            // Creates a Fatal Messsage
+            logger.LogMessage(LogLevel.FATAL, "This is a fatal message");
         }
     }
 }
