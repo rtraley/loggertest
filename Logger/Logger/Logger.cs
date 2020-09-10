9f0f5e8be4daa4eaae94de6f33fd8faafade8046
Richard Raley <RRaley@inboundtechnologies.com>
2020-09-10 08:31:45 -0500

    Creates log file directory if it does not exists

--- Logger/Logger/Logger.cs
+++ Logger/Logger/Logger.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 
 namespace RLog
 {
     public class Logger
     {
+        private readonly string logFilename;
+        private readonly string dateFormat;
+        /// <summary>
+        ///  Logs File  
+        /// </summary>
+        public Logger()
+        {
+            dateFormat = "yyyy-MM-dd HH:mm:ss.fff";
+            string logFolderPath = ConfigurationManager.AppSettings["LogFilePath"];
+            logFilename = logFolderPath + DateTime.Now.ToShortDateString() + ".log";
+            if (string.IsNullOrEmpty(logFolderPath))
+            {
+                logFolderPath = Environment.CurrentDirectory;
+            }
+            if (!Directory.Exists(logFolderPath))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(logFolderPath);
+            }
+        }
+
     }
 }
